ab8a8179bdc3f34f8fa0776abd1bf9647032ddbb
Vasco21 <vascoeti123@gmail.com>
2023-05-14 18:46:13 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
-# install -U Flask -v 2.1.0
+# install Flask -v 2.1.0
 exec { 'Flask':
-  command => '/usr/bin/apt-get -y install-U Flask -v 2.1.0',
+  command => '/usr/bin/apt-get -y install Flask -v 2.1.0',
 }
