e411a5d110e578de9770a53f05ce61ba886fbe6b
Vasco21 <vascoeti123@gmail.com>
2023-07-15 04:52:25 -0700

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,8 @@
-class custom_header {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  file { '/etc/nginx/conf.d/custom-header.conf':
-    content => 'add_header X-Served-By $hostname;',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx'],
-  }
+# creates a custom HTTP header response
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
-
-include custom_header
