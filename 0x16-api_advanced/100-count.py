ccc843d6bb45df87b742a4c78ff9decbc9513557
Vasco21 <vascoeti123@gmail.com>
2023-09-04 16:27:49 -0700

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,34 +1,48 @@
 #!/usr/bin/python3
-"""A file to make a query to an endpoint"""
-from requests import request
 
+import requests
 
-def count_words(subreddit, word_list, after="", counter={}, ini=0):
-    """A recursive function that queries the Reddit API, parses the title
-    of all hot articles, and prints a sorted count of given keywords
-    (case-insensitive, delimited by spaces. Javascript should count as
-    javascript, but java should not)
-    """
-    if ini == 0:
-        for word in word_list:
-            counter[word] = 0
+def count_words(subreddit, word_list, after=None, counts=None):
+    # Base case: If counts is None, initialize it as an empty dictionary
+    if counts is None:
+        counts = {}
 
-    url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
-    headers = {"User-Agent": "Python3"}
-    response = request("GET", url, headers=headers).json()
-    try:
-        top = response['data']['children']
-        _after = response['data']['after']
-        for item in top:
-            for word in counter:
-                counter[word] += item['data']['title'].lower(
-                    ).split(' ').count(word.lower())
-        if _after is not None:
-            count_words(subreddit, word_list, _after, counter, 1)
-        else:
-            str = sorted(counter.items(), key=lambda kv: kv[1], reverse=True)
-            for name, num in str:
-                if num != 0:
-                    print('{}: {}'.format(name, num))
-    except Exception:
-        return None
+    # Base case: If word_list is empty, there are no more keywords to count
+    if not word_list:
+        # Sort the counts first by count (descending), then alphabetically (ascending)
+        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_counts:
+            print(f"{word}: {count}")
+        return
+
+    # Define the Reddit API URL for the given subreddit and after parameter
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {'limit': 100, 'after': after}
+
+    # Set a custom User-Agent to avoid Too Many Requests error
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    # Send a GET request to the Reddit API
+    response = requests.get(url, headers=headers, params=params)
+
+    # Check if the response is successful (status code 200)
+    if response.status_code == 200:
+        data = response.json()
+        posts = data.get('data', {}).get('children', [])
+
+        # Extract titles from the posts and concatenate them
+        titles = [post['data']['title'] for post in posts]
+        title_text = ' '.join(titles)
+
+        # Loop through the word_list and count occurrences
+        keyword = word_list[0].lower()
+        keyword_count = title_text.lower().count(keyword)
+        if keyword_count > 0:
+            # Update the counts dictionary
+            counts[keyword] = counts.get(keyword, 0) + keyword_count
+
+        # Recursively call count_words with the rest of the word_list
+        count_words(subreddit, word_list[1:], data.get('data', {}).get('after'), counts)
+    else:
+        # If the subreddit is invalid or there was an error, print nothing
+        return
