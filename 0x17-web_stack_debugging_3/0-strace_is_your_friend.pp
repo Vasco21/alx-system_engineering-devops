a1e0f4f06ca6d68b06144025979d2237ef5244d6
Vasco21 <vascoeti123@gmail.com>
2023-09-04 17:20:37 -0700

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,6 @@
-strace -p <APACHE_PID> -e trace=file
+# Puppet manifest to fix a bug in wp-setings.php
+
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
