b06e58c09fa1a3c040b23e4d13205c2b5925cc1d
Vasco21 <vascoeti123@gmail.com>
2024-04-12 18:30:36 -0700

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,13 @@ Script that queries subscribers on a given Reddit subreddit.
 import requests
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'MyBot/0.0.1'}
-    response = requests.get(url, headers=headers)
+    url = "https://www.reddit.com/{}/about.json".format(subreddit)
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
         return 0
 
+    results = response.json().get("data")
+    return results.get('subscribers')
